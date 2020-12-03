e283150ce73d8a4cdc1ef7d4a7687a4a83e6a36a
weiweiudem <cvnwwei1@gmail.com>
2020-12-02 23:25:09 -0500

    new update encoder

--- clean.py
+++ clean.py
@@ -64,7 +64,7 @@ class Loader():
 
         # create_timpstamp_year
         self.df["creatTimestamp_year"] = self.df['creatTimestamp'].apply(lambda x: x.split()[-1]).tolist()
-        # self.df["creatTimestamp_year"] = self.df['creatTimestamp_year'].apply(lambda x: int(x))
+    
         # encode covImgStatus
         le.fit(self.df['covImgStatus'].unique())
         distr = le.classes_
